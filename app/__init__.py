a3f40aeac9f5df38ce7ca4ac30c4a3ff24b9f175
Lulurine <lulukatherine9527@gmail.com>
2025-06-15 17:48:24 +0800

    Remove unused code

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,5 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Load configuration from config.py
-app.config.from_pyfile('config.py')
-
 # Import routes
 from app import routes
--- app/config.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-class Config:
-    SECRET_KEY = 'your_secret_key_here'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///perfume.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-app = Flask(__name__)
-app.config.from_object(Config)
